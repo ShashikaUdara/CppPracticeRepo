9ab4afdf9eefba2d904f827c6f27cbccbed1bdfb
ShashikaUdara <techdudelab@gmail.com>
2022-03-07 23:03:03 +0530

    add oop directory

---
+++ practice oop/source/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	// public 		- members are accessible from outside the class
+	// private 		- members cannot be accessed (or viewed) from outside the class
+	// protected 	- members cannot be accessed from outside the class, however, they can be 
+	// 			  accessed in inherited classes. You will learn more about
+
+	cout<<"main here"<<endl;
+}
