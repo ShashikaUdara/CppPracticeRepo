8f6a122c6eebdd8a1dafabc36a3a5c90c25c8fe4
ShashikaUdara <techdudelab@gmail.com>
2022-04-10 19:54:29 +0530

    self referencing pointer

---
+++ C++AdvanceTopics/Self-referencing pointer/objMemberFunctionExamle.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+class C
+{
+	int _value = 0;
+public:
+	void setV(const int & val) {_value=val;}
+	int getV() const;
+	int getV2() const;
+};
+
+int C :: getV() const 
+{
+	printf("getv(): this- %p\n", this);
+	return _value;
+}
+
+int C :: getV2() const 
+{
+	printf("getv2(): this- %p\n", this);
+	return this->_value;
+}
+
+void print(const C & obj)
+{
+	printf("indirect- %d\n", obj.getV());
+}
+
+int main()
+{
+	const int i=90;
+	C o1;
+	o1.setV(i);
+	printf("address of o1 - %p\n", &o1);
+	int temp = o1.getV();
+	printf("getV()- %d\n", temp);
+	temp = o1.getV2();
+	printf("getV2()- %d\n", temp);
+}  
