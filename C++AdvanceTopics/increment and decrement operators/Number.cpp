02eda2837cc0d1a3acff56b0b1776f512f99912f
ShashikaUdara <techdudelab@gmail.com>
2022-04-20 01:59:52 +0530

    increment and decrement operator example completed

--- C++AdvanceTopics/increment and decrement operators/Number.cpp
+++ C++AdvanceTopics/increment and decrement operators/Number.cpp
@@ -15,17 +15,66 @@ public:
 	Number & operator ++ ();
 
 	// postfix increment operator - post-increment
-	Number & operator ++ (int);
+	Number operator ++ (int);
 
 	// prefix decrement operator - pre-decrement
 	Number & operator -- ();
 
 	// postfix decrement operator - post-decrement
-	Number & operator -- (int);
+	Number operator -- (int);
 
 };
+
+// prefix increment operator - pre-increment
+Number & Number :: operator ++ ()
+{
+	cout<<"prefix increment: ";
+	value += 1;
+	return *this;
+}
+
+// postfix increment operator - post-increment
+Number Number :: operator ++ (int)
+{
+	cout<<"postfix increment: ";
+	Number temp = *this;
+	value +=1;
+	return temp;
+}
+
+// prefix decrement operator - pre-decrement
+Number & Number :: operator -- ()
+{
+	cout<<"prefix decrement: ";
+	value -= 1;
+	return *this;
+}
+
+// postfix decrement operator - post-decrement
+Number Number :: operator -- (int)
+{
+	cout<<"postfix decrement: ";
+	Number temp = *this;
+	value -=1;
+	return temp;
+}
+
+ostream & operator <<  (ostream & o, const Number & n)
+{
+	return o << n.getValue();
+}
 
 int main()
 {
 	cout<<"it's main"<<endl;
+	Number num(64);
+	cout<<"Value is: "<<num<<endl;
+	cout<<"Value is: "<<num++<<endl;
+	cout<<"Value is: "<<num<<endl;
+	cout<<"Value is: "<<num--<<endl;
+	cout<<"Value is: "<<num<<endl;
+	cout<<"Value is: "<<++num<<endl;
+	cout<<"Value is: "<<num<<endl;
+	cout<<"Value is: "<<--num<<endl;
+	cout<<"Value is: "<<num<<endl;
 }
