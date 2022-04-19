9b914105836b62046955385aa2456916700620a7
ShashikaUdara <techdudelab@gmail.com>
2022-04-19 23:55:37 +0530

    more increment and decrimant operators

--- C++AdvanceTopics/increment and decrement operators/Number.cpp
+++ C++AdvanceTopics/increment and decrement operators/Number.cpp
@@ -10,6 +10,19 @@ public:
 	Number(int val = 0):value(val){}
 	int getValue() const { return value; }
 	void setValue(int v) { value = v; }
+
+	// prefix increment operator - pre-increment
+	Number & operator ++ ();
+
+	// postfix increment operator - post-increment
+	Number & operator ++ (int);
+
+	// prefix decrement operator - pre-decrement
+	Number & operator -- ();
+
+	// postfix decrement operator - post-decrement
+	Number & operator -- (int);
+
 };
 
 int main()
