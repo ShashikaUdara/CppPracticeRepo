e04650658c2b96dde87197ee82e97cb63bfe5695
ShashikaUdara <techdudelab@gmail.com>
2022-05-01 23:53:01 +0530

    Quick Sort - bugs fixed

--- ds/Quick Sort/Quick.cpp
+++ ds/Quick Sort/Quick.cpp
@@ -8,7 +8,7 @@ void Quick :: quickSort(int * arr, int low, int high)
 		// cout<<"4"<<endl;
 		pi = partition(arr, low, high);
 		// cout<<"5"<<endl;
-		quickSort(arr, low, pi);
+		quickSort(arr, low, pi-1);
 		// cout<<"6"<<endl;
 		quickSort(arr, pi+1, high);
 	}
@@ -16,11 +16,11 @@ void Quick :: quickSort(int * arr, int low, int high)
 
 int Quick :: partition(int * arr, int low, int high)
 {
-	int i = low - 1, j = 0, iTemp = 0, iPivote = arr[high - 1];
-	cout<<"5 1"<<endl;
-	for(j=0; j< high; j++)
+	int i = low - 1, j = 0, iTemp = 0, iPivote = arr[high];
+	// cout<<"5 1"<<endl;
+	for(j=low; j<= high-1; j++)
 	{
-		cout<<"5 2"<<endl;
+		// cout<<"5 2"<<endl;
 		if(arr[j] <= iPivote)
 		{
 			i++;
@@ -29,14 +29,14 @@ int Quick :: partition(int * arr, int low, int high)
 			arr[i] = arr[j];
 			arr[j] = iTemp;
 		}
-		cout<<"5 3"<<endl;
+		// cout<<"5 3"<<endl;
 	}
-	cout<<"5 4"<<endl;
+	// cout<<"5 4"<<endl;
 	i++;
 	iTemp = arr[i];
-	arr[i] = arr[j];
-	arr[j] = iTemp;
+	arr[i] = arr[high];
+	arr[high] = iTemp;
 
-	cout<<"5 5"<<endl;
+	cout<<"5 5 i = "<<i<<endl;
 	return i; // next pivote value
 }
--- ds/Quick Sort/main.cpp
+++ ds/Quick Sort/main.cpp
@@ -5,11 +5,20 @@ using namespace std;
 
 int main()
 {
-	cout<<"it's main"<<endl;
 	int arr[] = {6, 4, 2, 1, 5, 9, 8};
-	cout<<"1"<<endl;
+	int it = 0;
+	for(it=0; it<7; it++)
+	{
+		cout<<arr[it]<<" - ";
+	}
+	cout<<endl;
+
 	Quick * quick = new Quick;
-	cout<<"2"<<endl;
-	quick->quickSort(arr, 0, 7);
-	cout<<"3"<<endl;
+	quick->quickSort(arr, 0, 6);
+
+	for(it=0; it<7; it++)
+	{
+		cout<<arr[it]<<" - ";
+	}
+	cout<<endl;
 }
